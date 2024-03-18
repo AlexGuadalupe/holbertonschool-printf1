9f837cd443d2120c8ce9c586f089bc19426c7fd3
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-18 17:25:48 -0400

    files set names

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
+
+/** 
+* _printf: printf
+* @char: variable tu print.
+*/
+
+int _printf(const char *format, ...)
+va_list args;
+va_start(args, format);
+{
+if()
+}
