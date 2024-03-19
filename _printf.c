d7e34237ea8efb4547731a30b07ec516392f56af
Alex Guadalupe <9035@holbertonstudents.com>
2024-03-19 17:44:05 -0400

    update betty style

--- _printf.c
+++ _printf.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 
-/** 
-* _printf: printf
-* @char: variable tu print.
+/**
+* _printf: printf.
+* @format: variable tu print.
+* @args: arguments.
 */
 
 int _printf(const char *format, ...)
@@ -13,43 +14,44 @@ va_list args;
 unsigned int count = 0;
 va_start(args, *format);
 
-while(*format)
+while (*format)
 {
-	if(*format == '%')
-	{
-		format++;
+if (*format == '%')
+{
+format++;
 
-		if(*format == 'c')
-        {
-            putchar(va_arg(args, int));
-            count++;
-        }
-        if(*format =='s')
-        {
-            char *str = va_arg(args, char *);
-            if(str == NULL)
-				{
-                count+= printf("(null)");
-				}
-			else{
-			while(*str)
-            {
-                putchar(*str);
-                count++;
-                str++;
-            }
-    }
-        }
-        if(*format == 'd')
-        {
-            putchar(va_arg(args, int));
-            count++;
-        }
-        else if(*format == 'i')
-        {
-            putchar(va_arg(args, int));
-            count++;
-        }
+if (*format == 'c')
+{
+putchar(va_arg(args, int));
+count++;
+}
+if (*format == 's')
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+{
+count += printf("(null)");
+}
+else
+{
+while (*str)
+{
+putchar(*str);
+count++;
+str++;
+}
+}
+}
+if (*format == 'd')
+{
+putchar(va_arg(args, int));
+count++;
+}
+else if (*format == 'i')
+{
+putchar(va_arg(args, int));
+count++;
+}
 
 putchar(*format++);
 count++;
